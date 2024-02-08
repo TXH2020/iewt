64ffaae6b7b5158d7b0a6d8bfd23c56060a28b2d
TXH2020 <87963860+TXH2020@users.noreply.github.com>
2024-02-08 21:50:13 +0530

    Update load_testing.py

--- load_test/load_testing.py
+++ load_test/load_testing.py
@@ -2,9 +2,11 @@ import aiohttp
 import asyncio
 from websocket import create_connection
 import select
+from timeit import default_timer as timer
 import time
 import json
 import ast
+import psutil
 wsconns=[]
 
 def format(string):
@@ -15,29 +17,37 @@ def format(string):
 async def websocket_connection():
     global wsconns
     async with aiohttp.ClientSession() as session:
-        h,u,p=('20.83.180.145','iewt','Juo987Hj*67jkiuop')
-        url='http://localhost:8888/index?hostname={0}&username={1}&password={2}'.format(h,u,p)
+        h,u,p=('osboxes','osboxes','osboxes.org')
+        url='http://localhost:8888?hostname={0}&username={1}&password={2}'.format(h,u,p)
         async with session.post(url) as response:
             dictionary = await response.json()
-            ws = create_connection("ws://localhost:8888/index/ws?id="+dictionary.get('id'))
-            while(True):
-                r,_,_=select.select([ws],[],[])
-                if r:
-                    break
+            ws = create_connection("ws://localhost:8888/ws?id="+dictionary.get('id'))
             wsconns.append(ws)
-            print("Connection dict:", dictionary)
+
+
+
+PROCNAME = "iewt"
+pid=None
+for proc in psutil.process_iter():
+    if proc.name() == PROCNAME:
+        pid=proc.pid
+iewt_proc=psutil.Process(pid)
 
 
 n=int(input("Enter no. of connections:"))
 success_count=0
 for i in range(n):
-   asyncio.run(websocket_connection())
-   success_count+=1
+    try:
+        start = timer()
+        asyncio.run(websocket_connection())
+        end = timer()
+        success_count+=1
+        print("Time=",(end-start),"s","\t",success_count)
+        if(i%100==0):
+            print("Memory%=",iewt_proc.memory_percent(),"CPU%=",(iewt_proc.cpu_percent() / psutil.cpu_count()))
+    except Exception as e:
+        print(e)
+
 print("No. of successful connections=",success_count)
-time.sleep(2)
-for i in range(success_count):
-    wsconns[i].send(format('top\r'))
-print("Command top sent to all connections")
-time.sleep(10)
 for i in range(success_count):
     wsconns[i].close()
